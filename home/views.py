1675ce0a66373622c32f770a1d43d858d371d16b
sanson0 <s04anson@gmail.com>
2021-12-13 21:25:01 +0000

    Added contact and delivery templates, views, urls

--- home/views.py
+++ home/views.py
@@ -7,3 +7,11 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+
+def deliver(request):
+    return render(request, 'home/deliver.html')
