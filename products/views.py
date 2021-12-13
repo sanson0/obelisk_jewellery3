444a7958d7764519981077bdb410a6c50165e9dc
sanson0 <s04anson@gmail.com>
2021-12-13 14:07:47 +0000

    Product admin, added product view, url and template

--- products/views.py
+++ products/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Product, Category
+from .forms import ProductForm
 
+# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -67,3 +68,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
