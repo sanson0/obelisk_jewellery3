2a979a2d875bf568e6d1c510a9150c8ad9f5e55a
sanson0 <s04anson@gmail.com>
2021-12-22 12:58:28 +0000

    Python code improvements 2

--- checkout/views.py
+++ checkout/views.py
@@ -71,7 +71,8 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in \
+                                item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -112,7 +113,7 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info 
+        # Attempt to prefill the form with any info
         # the user maintains in their profile
         if request.user.is_authenticated:
             try:
