8c30f6b6d8691c28b50629290702ae75425011bc
sanson0 <s04anson@gmail.com>
2021-12-23 01:24:26 +0000

    Debug with settings.py

--- obelisk_jewellery/settings.py
+++ obelisk_jewellery/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-
+DEBUG = True
 ALLOWED_HOSTS = ['obelisk-jewels.herokuapp.com', 'localhost']
 
 
@@ -205,9 +205,9 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'obeliskjewellery@example.com'
-else:
+#    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#    DEFAULT_FROM_EMAIL = 'obeliskjewellery@example.com'
+# else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
